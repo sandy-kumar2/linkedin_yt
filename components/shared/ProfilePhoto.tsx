c0dfa10f7e3d868bf80c881a32f50c6ec037df4f
sandy-kumar2 <imsandeep721@gmail.com>
2024-07-03 17:11:18 +0530

    updated

---
+++ components/shared/ProfilePhoto.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Avatar, AvatarImage } from '../ui/avatar'
+
+const ProfilePhoto = ({ src }: { src: string }) => {
+    return (
+        <div>
+            <Avatar className='cursor-pointer'>
+                <AvatarImage src={src} alt="banner" />
+            </Avatar>
+        </div>
+    )
+}
+
+export default ProfilePhoto
