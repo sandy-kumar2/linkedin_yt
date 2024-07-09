e4b126bcc09043c17c55537ff75fff69321982e5
sandy-kumar2 <imsandeep721@gmail.com>
2024-07-10 00:04:59 +0530

    updated

--- lib/serveractions.ts
+++ lib/serveractions.ts
@@ -1,12 +1,11 @@
 "use server"
-
 import { Post } from "@/models/post.model";
 import { IUser } from "@/models/user.model";
 import { currentUser } from "@clerk/nextjs/server"
 import { v2 as cloudinary } from 'cloudinary';
 import connectDB from "./db";
 import { revalidatePath } from "next/cache";
-import { Comment } from "@/models/comment.model";
+import { Comment, ICommentDocument } from "@/models/comment.model";
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -14,15 +13,13 @@ cloudinary.config({
     api_secret: process.env.API_SECRET
 });
 
-// creating post using server actions
 export const createPostAction = async (inputText: string, selectedFile: string) => {
     await connectDB();
     const user = await currentUser();
-    if (!user) throw new Error('User not athenticated');
+    if (!user) throw new Error('User not authenticated');
     if (!inputText) throw new Error('Input field is required');
 
     const image = selectedFile;
-
 
     const userDatabase: IUser = {
         firstName: user.firstName || "Patel",
@@ -33,26 +30,24 @@ export const createPostAction = async (inputText: string, selectedFile: string) 
     let uploadResponse;
     try {
         if (image) {
-            //1. create post with image
             uploadResponse = await cloudinary.uploader.upload(image);
             await Post.create({
                 description: inputText,
                 user: userDatabase,
-                imageUrl: uploadResponse?.secure_url // yha pr image url ayega from cloudinary
-            })
+                imageUrl: uploadResponse?.secure_url
+            });
         } else {
-            //2. create post with text only
             await Post.create({
                 description: inputText,
                 user: userDatabase
-            })
+            });
         }
         revalidatePath("/");
     } catch (error: any) {
         throw new Error(error);
     }
 }
-// get all post using server actions
+
 export const getAllPosts = async () => {
     try {
         await connectDB();
@@ -64,7 +59,6 @@ export const getAllPosts = async () => {
     }
 }
 
-// delete post by id
 export const deletePostAction = async (postId: string) => {
     await connectDB();
     const user = await currentUser();
@@ -72,7 +66,6 @@ export const deletePostAction = async (postId: string) => {
     const post = await Post.findById(postId);
     if (!post) throw new Error('Post not found.');
 
-    // keval apni hi post delete kr payega.
     if (post.user.userId !== user.id) {
         throw new Error('You are not an owner of this Post.');
     }
@@ -86,6 +79,7 @@ export const deletePostAction = async (postId: string) => {
 
 export const createCommentAction = async (postId: string, formData: FormData) => {
     try {
+        await connectDB();
         const user = await currentUser();
         if (!user) throw new Error("User not authenticated");
         const inputText = formData.get('inputText') as string;
@@ -98,10 +92,10 @@ export const createCommentAction = async (postId: string, formData: FormData) =>
             userId: user.id,
             profilePhoto: user.imageUrl
         }
-        const post = await Post.findById({ _id: postId });
+        const post = await Post.findById(postId);
         if (!post) throw new Error('Post not found');
 
-        const comment = await Comment.create({
+        const comment: ICommentDocument = await Comment.create({
             textMessage: inputText,
             user: userDatabase,
         });
@@ -110,7 +104,7 @@ export const createCommentAction = async (postId: string, formData: FormData) =>
         await post.save();
 
         revalidatePath("/");
-    } catch (error) {
-        throw new Error('An error occurred')
+    } catch (error: any) {
+        throw new Error('An error occurred');
     }
 }
