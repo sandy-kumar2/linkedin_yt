2196369e10cb4214d4443be75e43a705f087705b
sandy-kumar2 <imsandeep721@gmail.com>
2024-07-10 01:41:00 +0530

    Update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
             <div className=' flex items-center max-w-6xl justify-between h-14 mx-auto px-3'>
                 <div className='flex items-center gap-2'>
                     <Image
-                        src={'/LinkedIn_logo.png'}
+                        src={'/linkedin.png'}
                         alt="Logo"
                         width={35}
                         height={35}
