b8051700313b2244be7a3d398aa3d34fbbddb759
sandy-kumar2 <imsandeep721@gmail.com>
2024-07-10 01:21:19 +0530

    update

--- components/SocialOptions.tsx
+++ components/SocialOptions.tsx
@@ -7,7 +7,7 @@ import { useUser } from '@clerk/nextjs';
 import CommentInput from './CommentInput';
 import Comments from './Comments';
 
-const SocialOptions = ({ post }: { post: IPostDocument, postId: string }) => {
+const SocialOptions = ({ post }: { post: IPostDocument }) => {
   const { user } = useUser();
   const [liked, setLiked] = useState(false);
   const [likes, setLikes] = useState(post.likes);
