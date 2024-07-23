7de77a9d3c988519cb1cf62c29bec066029e2012
sandy-kumar2 <imsandeep721@gmail.com>
2024-07-23 21:05:45 +0530

    update

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,6 +1,7 @@
 import { IPostDocument } from '@/models/post.model'
 import React from 'react'
 import Comment from './Comment' 
+
 const Comments = ({post}:{post:IPostDocument}) => {
   return (
     <div> 
--- components/Posts.tsx
+++ components/Posts.tsx
@@ -3,7 +3,6 @@ import Post from './Post'
 import { IPostDocument } from '@/models/post.model'
 
 const Posts = ({ posts }: { posts: IPostDocument[] }) => {
-  
   return (
     <div>
       {
